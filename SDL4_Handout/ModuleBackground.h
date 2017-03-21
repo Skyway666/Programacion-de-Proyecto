736c129b709d1b09028d41ca96f8d89c4a7976ac
Skyway666 <lucasgarciamateu@gmail.com>
2017-03-21 12:38:09 +0100

    street fighter

---
+++ SDL4_Handout/ModuleBackground.h
@@ -0,0 +1,33 @@
+#ifndef __ModuleBackground_H__
+#define __ModuleBackground_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+struct SDL_Texture;
+
+class ModuleBackground : public Module
+{
+public:
+	ModuleBackground();
+	~ModuleBackground();
+
+	bool Start();
+	update_status Update();
+
+public:
+	
+	SDL_Texture* graphics = nullptr;
+	SDL_Rect ground;
+	SDL_Rect ship;
+	SDL_Rect background;
+	Animation flag;
+	Animation cheer_leader;
+
+	float ship_movy = 0;
+	int sense = 0;
+
+};
+
+#endif
